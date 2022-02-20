6e6fa8745a0e40c6cffa9251f89c4469016ee503
panoramix360 <lucasreis3000@gmail.com>
2022-02-20 19:34:47 -0300

    rudimentary router

--- hello-world.js
+++ hello-world.js
@@ -1,9 +1,25 @@
 const http = require('http')
 const port = process.env.PORT || 3000
 
+const urlPattern = /\/?(?:\?.*)?$/
+
 const server = http.createServer((req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text/plain' })
-    res.end('Hello World')
+  const path = req.url.replace(urlPattern, '').toLowerCase()
+
+  switch (path) {
+    case '':
+      res.writeHead(200, { 'Content-Type': 'text/plain' })
+      res.end('Home')
+      break
+    case '/about':
+      res.writeHead(200, { 'Content-Type': 'text/plain' })
+      res.end('About')
+      break
+    default:
+      res.writeHead(404, { 'Content-Type': 'text/plain' })
+      res.end('Not Found')
+      break
+  }
 })
 
 server.listen(port, () => console.log(`server started on port ${port}; press Ctrl-C to terminate...`))
