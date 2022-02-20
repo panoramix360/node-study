64efc22134daf3cc20a7b2fac136dbc610d7da4f
panoramix360 <lucasreis3000@gmail.com>
2022-02-20 20:14:12 -0300

    creating brazil travel project

---
+++ brazil-travel/site/server.js
@@ -0,0 +1,21 @@
+const express = require('express')
+
+const app = express()
+
+const port = process.env.PORT || 3000
+
+app.use((req, res) => {
+  res.type('text/plain')
+  res.status(404)
+  res.send('404 - Not Found')
+})
+
+// custom 500 page
+app.use((err, req, res, next) => {
+  console.error(err.message)
+  res.type('text/plain')
+  res.status(500)
+  res.send('500 - Internal Server Error')
+})
+
+app.listen(port, () => console.log(`Express started on http://localhost:${port}; press Ctrl-C to terminate.`))
