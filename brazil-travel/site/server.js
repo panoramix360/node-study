6ed859f401eff8806df7c0243ac1ff7c4c6776e0
panoramix360 <lucasreis3000@gmail.com>
2022-02-20 20:53:07 -0300

    adding express handlebars for view templates

--- brazil-travel/site/server.js
+++ brazil-travel/site/server.js
@@ -1,31 +1,30 @@
 const express = require('express')
+const expressHandlebars = require('express-handlebars')
 
 const app = express()
 
 const port = process.env.PORT || 3000
 
-app.get('/', (req, res) => {
-  res.type('text/plain')
-  res.send('Brazil Travel')
-})
+app.engine('handlebars', expressHandlebars.engine({
+  defaultLayout: 'main'
+}))
 
-app.get('/about', (req, res) => {
-  res.type('text/plain')
-  res.send('About Brazil Travel')
-})
+app.set('view engine', 'handlebars')
+
+app.get('/', (req, res) => res.render('home'))
+
+app.get('/about', (req, res) => res.render('about'))
 
 app.use((req, res) => {
-  res.type('text/plain')
   res.status(404)
-  res.send('404 - Not Found')
+  res.render('404')
 })
 
 // custom 500 page
 app.use((err, req, res, next) => {
   console.error(err.message)
-  res.type('text/plain')
   res.status(500)
-  res.send('500 - Internal Server Error')
+  res.send('500')
 })
 
 app.listen(port, () => console.log(`Express started on http://localhost:${port}; press Ctrl-C to terminate.`))
