a90b5c8e2332d6da0cf214df2d1ff96bcbf14d0a
panoramix360 <lucasreis3000@gmail.com>
2022-02-20 21:19:09 -0300

    creating readme and a dynamic about page

--- brazil-travel/site/server.js
+++ brazil-travel/site/server.js
@@ -5,15 +5,28 @@ const app = express()
 
 const port = process.env.PORT || 3000
 
+const fortunes = [
+  "Conquer your fears or they will conquer you.",
+  "Rivers need springs.",
+  "Do not fear what you don't know.",
+  "You will have a pleasant surprise.",
+  "Whenever possible, keep it simple.",
+]
+
 app.engine('handlebars', expressHandlebars.engine({
   defaultLayout: 'main'
 }))
 
 app.set('view engine', 'handlebars')
 
+app.use(express.static(`${__dirname}/public`))
+
 app.get('/', (req, res) => res.render('home'))
 
-app.get('/about', (req, res) => res.render('about'))
+app.get('/about', (req, res) => {
+  const randomFortune = fortunes[Math.floor(Math.random() * fortunes.length)]
+  res.render('about', { fortune: randomFortune })
+})
 
 app.use((req, res) => {
   res.status(404)
