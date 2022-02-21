d07fd9ffaa478a3723c2d245b9bf9752b477dc38
panoramix360 <lucasreis3000@gmail.com>
2022-02-20 22:41:06 -0300

    adding puppeter to test UI

--- brazil-travel/site/server.js
+++ brazil-travel/site/server.js
@@ -20,4 +20,10 @@ app.get('/about', handlers.about)
 app.use(handlers.notFound)
 app.use(handlers.serverError)
 
-app.listen(port, () => console.log(`Express started on http://localhost:${port}; press Ctrl-C to terminate.`))
+if (require.main === module) {
+  app.listen(port, () => {
+    console.log(`Express started on http://localhost:${port}; press Ctrl-C to terminate.`)
+  })
+} else {
+  module.exports = app
+}
